c9161e948f585ad24d86407ab9796df738f4e79f
Soumyadeb2000 <soumyadebsutradhar@gmail.com>
2024-02-07 18:20:25 +0530

    changed purchase logic

--- Controllers/Purchase.js
+++ Controllers/Purchase.js
@@ -6,14 +6,16 @@ const Voucher = require('../Models/Vouchers');
 
 exports.buyVoucher = async (req, res) => {
     try {
-        const { code, userId } = req.body;
-        const voucher = await Voucher.findOne({ where: { code: code }});
-        if (!voucher) {
-            req.status(404).json({ failed: 'Invalid Voucher Code' });
+        const { voucherId, userId } = req.body;
+        const voucher = await Voucher.findOne({ where: { id: voucherId }});
+        if (!voucher || !voucher.isValid) {
+            res.status(404).json({ failed: 'Invalid Voucher Code' });
         } else {
             const user = await User.findOne({where: {id: userId*1}});
             if(user) {
-                await UserVoucher.create({userId, voucherId: voucher.id })
+                await UserVoucher.create({userId, voucherId})
+                voucher.isValid = false;
+                voucher.save();
                 res.status(200).json({ success: 'Voucher added to User' });
             } else {
                 res.status(404).json({ failed: 'User not found' });
@@ -26,16 +28,16 @@ exports.buyVoucher = async (req, res) => {
 
 exports.deleteVoucher = async (req, res) => {
     try {
-        const { code, userId } = req.body;
-        const voucher = await Voucher.findOne({ where: { code: code }, attributes: ['id']});
-        if (!voucher) {
-            req.status(404).json({ failed: 'Invalid Voucher Code' });
+        const { voucherId, userId } = req.body;
+        const voucher = await Voucher.findOne({ where: { id: voucherId }, attributes: ['id','isValid']});
+        if (!voucher || !voucher.isValid) {
+            res.status(404).json({ failed: 'Invalid Voucher Code' });
         } else {
             const user = await User.findOne({where: {id: userId*1}, attributes: ['id']});
             if(user) {
-                const userHasVoucher = UserVoucher.findOne({where: {userId: userId, voucherId: voucher.id}})
+                const userHasVoucher = UserVoucher.findOne({where: {userId, voucherId}})
                 if(userHasVoucher) {
-                    await UserVoucher.destroy({where: {userId: userId, voucherId: voucher.id}});
+                    await UserVoucher.destroy({where: {userId, voucherId}});
                     res.status(200).json({ success: 'Voucher deleted for the user' });
                 } else {
                     res.status(404).json({ failed: 'User doesn\'t have the voucher' });
@@ -56,8 +58,8 @@ exports.getVouchers = async (req, res) => {
         if (!user) {
             res.status(404).json({ failed: 'Invalid User' });
         } else {
-            const vouchers = await UserVoucher.findAll({where: {userId: id}});
-            res.status(200).json({vouchers});
+            const vouchersAndUsers = await UserVoucher.findAll({where: {userId: id}});
+            res.status(200).json({vouchersAndUsers});
         }
     } catch (error) {
         res.status(500).json({ error: error.message });
--- Routes/Purchase.js
+++ Routes/Purchase.js
@@ -4,7 +4,7 @@ const purchaseControllers = require('../Controllers/Purchase');
 
 router.post('/buy', purchaseControllers.buyVoucher);
 
-router.post('/delete', purchaseControllers.deleteVoucher);
+router.delete('/delete', purchaseControllers.deleteVoucher);
 
 router.get('/:id', purchaseControllers.getVouchers);
 
